b178b7bf73045b58c30104796fd06b13f0eb05f3
Karl Hepworth <karl.hepworth@gmail.com>
2017-10-16 21:55:37 +1100

    Contributes to #100

--- version/legacy.go
+++ version/legacy.go
@@ -1,1 +1,52 @@
 package version
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"os/exec"
+	"os/user"
+)
+
+// LegacyInstall is basically the main() func for Legacy versions which encapsulates
+// the code/dependencies for installing legacy Drush versions.
+// This function is deprecated and unused.
+func (drushVersion *DrushVersion) LegacyInstall() {
+	drushVersion.LegacyInstallVersion()
+	drushVersion.LegacyInstallTable()
+}
+
+// LegacyInstallTable is essentially always missing from older Drush versions.
+// This ensures the script is available to the legacy version.
+// This function is deprecated and unused.
+func (drushVersion *DrushVersion) LegacyInstallTable() {
+	usr, _ := user.Current()
+	log.Infoln("Fixing dependency issue with module Console_Table")
+	ctFileName := "Table.inc"
+	ctRemotePath := "https://raw.githubusercontent.com/pear/Console_Table/master/Table.php"
+	ctPath := usr.HomeDir + "/.dvm/versions/drush-" + drushVersion.version + "/includes/"
+	ctFile := ctPath + ctFileName
+	_, wgetErr := exec.Command("sh", "-c", "wget", ctRemotePath).Output()
+	if wgetErr != nil {
+		log.Infoln("wget returned error:", wgetErr)
+	}
+	exec.Command("mv ./" + ctFileName + " " + ctFile).Run()
+}
+
+// LegacyInstallVersion will install from a zip file which was located via git tags (manual input see ListLocal()).
+// This function is deprecated and unused.
+func (drushVersion *DrushVersion) LegacyInstallVersion() {
+	usr, _ := user.Current()
+	log.Infoln("Downloading and extracting legacy Drush version ", drushVersion.version)
+	zipFileName := drushVersion.version + ".zip"
+	remotePath := "https://github.com/drush-ops/drush/archive/" + zipFileName
+	zipPath := usr.HomeDir + "/.dvm/versions/"
+	zipFile := zipPath + zipFileName
+	exec.Command("sh", "-c", "mkdir -p "+zipPath).Run()
+	_, wgetErr := exec.Command("sh", "-c", "wget", remotePath).Output()
+	if wgetErr != nil {
+		log.Warnln("wget returned error:", wgetErr)
+	}
+	exec.Command("sh", "-c", "mv "+zipFileName+" "+zipPath).Run()
+	exec.Command("sh", "-c", "cd "+zipPath+" && unzip "+zipFile).Run()
+	exec.Command("sh", "-c", "rm -f "+zipFile).Run()
+	drushVersion.Status()
+}
--- version/version.go
+++ version/version.go
@@ -76,50 +76,6 @@ func (drushVersion *DrushVersion) Status() bool {
 	return false
 }
 
-// LegacyInstall is basically the main() func for Legacy versions which encapsulates
-// the code/dependencies for installing legacy Drush versions.
-func (drushVersion *DrushVersion) LegacyInstall() {
-	drushVersion.LegacyInstallVersion()
-	drushVersion.LegacyInstallTable()
-}
-
-// LegacyInstallTable is essentially always missing from older Drush versions.
-// This ensures the script is available to the legacy version.
-func (drushVersion *DrushVersion) LegacyInstallTable() {
-	// @TODO: Restore functionality in the Golang way...
-	//usr, _ := user.Current()
-	//log.Infoln("Fixing dependency issue with module Console_Table")
-	//ctFileName := "Table.inc"
-	//ctRemotePath := "https://raw.githubusercontent.com/pear/Console_Table/master/Table.php"
-	//ctPath := usr.HomeDir + "/.dvm/versions/drush-" + drushVersion.version + "/includes/"
-	//ctFile := ctPath + ctFileName
-	//_, wgetErr := exec.Command("sh", "-c", "wget", ctRemotePath).Output()
-	//if wgetErr != nil {
-	//	log.Infoln("wget returned error:", wgetErr)
-	//}
-	//exec.Command("mv ./" + ctFileName + " " + ctFile).Run()
-}
-
-// LegacyInstallVersion will install from a zip file which was located via git tags (manual input see ListLocal()).
-func (drushVersion *DrushVersion) LegacyInstallVersion() {
-	// @TODO: Rewrite in the Golang way.
-	usr, _ := user.Current()
-	log.Infoln("Downloading and extracting legacy Drush version ", drushVersion.version)
-	zipFileName := drushVersion.version + ".zip"
-	remotePath := "https://github.com/drush-ops/drush/archive/" + zipFileName
-	zipPath := usr.HomeDir + "/.dvm/versions/"
-	zipFile := zipPath + zipFileName
-	exec.Command("sh", "-c", "mkdir -p "+zipPath).Run()
-	_, wgetErr := exec.Command("sh", "-c", "wget", remotePath).Output()
-	if wgetErr != nil {
-		log.Warnln("wget returned error:", wgetErr)
-	}
-	exec.Command("sh", "-c", "mv "+zipFileName+" "+zipPath).Run()
-	exec.Command("sh", "-c", "cd "+zipPath+" && unzip "+zipFile).Run()
-	exec.Command("sh", "-c", "rm -f "+zipFile).Run()
-	drushVersion.Status()
-}
-
 // Install will install a version of drush version with composer in a common location.
 func (drushVersion *DrushVersion) Install() {
 	assertFileSystem()
